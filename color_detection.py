9353953f402393862a34fce41d1583b261df8ac2
NandiSoham <nandisoham21@gmail.com>
2023-10-31 13:19:20 -0700

    fixed few issues with the camera feed and hsv dragtracker

--- color_detection.py
+++ color_detection.py
@@ -1,18 +1,22 @@
 import cv2
 import numpy as np
 
+
 def create_color_detection_window():
     cv2.namedWindow('Color Detection')
     cv2.createTrackbar('Hue', 'Color Detection', 0, 179, on_trackbar_change)
-    cv2.createTrackbar('Saturation', 'Color Detection', 0, 255, on_trackbar_change)
+    cv2.createTrackbar('Saturation', 'Color Detection',
+                       0, 255, on_trackbar_change)
     cv2.createTrackbar('Value', 'Color Detection', 0, 255, on_trackbar_change)
+
 
 def on_trackbar_change(x):
     pass
 
+
 def get_color_mask(hue, saturation, value, frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
+
     lower_color = np.array([hue, saturation, value])
     upper_color = np.array([180, 255, 255])
 
@@ -20,6 +24,7 @@ def get_color_mask(hue, saturation, value, frame):
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
     return result
+
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -31,7 +36,6 @@ def main():
         if not ret:
             print("Failed to grab frame")
             break
-
 
         height, width, _ = frame.shape
         height, width, _ = frame.shape
@@ -45,18 +49,14 @@ def main():
         value = cv2.getTrackbarPos('Value', 'Color Detection')
         result = get_color_mask(hue, saturation, value, frame)
 
-        cv2.imshow('Color Detection', frame)
-
-        frame = cv2.rectangle(frame, (0, 0), (width, 2*box_height), (0, 255, 0), 2)
-        frame = cv2.rectangle(frame, (0, 2*box_height), (width, height), (0, 255, 0), 2)
-
-        cv2.imshow('Camera Feed', frame)
+        cv2.imshow('Color Detection', result)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
