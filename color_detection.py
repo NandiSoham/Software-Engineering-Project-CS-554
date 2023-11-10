fe6354f05edfeb10c742fb5c04dff9ac2216a3a5
shirish <mankal@pdx.edu>
2023-11-09 19:17:47 -0800

    Unit testing for the get_color_mask function in the color detection file.

--- color_detection.py
+++ color_detection.py
@@ -1,30 +1,20 @@
 import cv2
 import numpy as np
 
-
 def create_color_detection_window():
     cv2.namedWindow('Color Detection')
     cv2.createTrackbar('Hue', 'Color Detection', 0, 179, on_trackbar_change)
-    cv2.createTrackbar('Saturation', 'Color Detection',
-                       0, 255, on_trackbar_change)
+    cv2.createTrackbar('Saturation', 'Color Detection', 0, 255, on_trackbar_change)
     cv2.createTrackbar('Value', 'Color Detection', 0, 255, on_trackbar_change)
-
 
 def on_trackbar_change(x):
     pass
 
-
 def get_color_mask(hue, saturation, value, frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-<<<<<<< HEAD
     
     lower_color = np.array([hue, saturation, value], dtype=np.uint8)
-    upper_color = np.array([180, 255, 255], dtype=np.uint8) 
-=======
-
-    lower_color = np.array([hue, saturation, value])
-    upper_color = np.array([180, 255, 255])
->>>>>>> 80ae4b6dcdea0a2ed8c68c6a2e51ae6ef7fcc742
+    upper_color = np.array([180, 255, 255], dtype=np.uint8)  
 
     mask = cv2.inRange(hsv, lower_color, upper_color)
     result = cv2.bitwise_and(frame, frame, mask=mask)
@@ -43,6 +33,7 @@ def main():
             print("Failed to grab frame")
             break
 
+
         height, width, _ = frame.shape
         height, width, _ = frame.shape
 
@@ -55,23 +46,18 @@ def main():
         value = cv2.getTrackbarPos('Value', 'Color Detection')
         result = get_color_mask(hue, saturation, value, frame)
 
-<<<<<<< HEAD
         cv2.imshow('Color Detection', frame)
     
         frame = cv2.rectangle(frame, (0, 0), (width, 2*box_height), (0, 255, 0), 2)
         frame = cv2.rectangle(frame, (0, 2*box_height), (width, height), (0, 255, 0), 2)
 
         cv2.imshow('Camera Feed', frame)
-=======
-        cv2.imshow('Color Detection', result)
->>>>>>> 80ae4b6dcdea0a2ed8c68c6a2e51ae6ef7fcc742
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     main()
