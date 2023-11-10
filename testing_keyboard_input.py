43baa6f553486c3faf853da992344cd950ff016c
HimaPratyusha <svhimapratyusha@gmail.com>
2023-11-09 17:06:29 -0800

    changes the testing code and performed unit testing

--- testing_keyboard_input.py
+++ testing_keyboard_input.py
@@ -4,6 +4,12 @@ from keyboard_input_simulator import press_key, release_key, KEY_W, KEY_A, KEY_D
 
 class TestKeyboardFunctions(unittest.TestCase):
     
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
     def test_press_and_release_key(self):
         press_key(KEY_W)
         time.sleep(1)
