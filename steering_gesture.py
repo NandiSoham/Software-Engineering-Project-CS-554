5169f633dfa9f3319b1c799beca3262f643e42bf
HimaPratyusha <svhimapratyusha@gmail.com>
2023-11-14 01:19:23 -0800

    worked on gaussian blur and contour finding to reduce the video noise

--- steering_gesture.py
+++ steering_gesture.py
@@ -2,7 +2,7 @@ import cv2
 import imutils
 import threading
 from imutils.video import VideoStream
-
+from keyboard_input_simulator import PressKey, ReleaseKey, A, D, Space
 
 stop_event = threading.Event()
 
@@ -14,9 +14,38 @@ def image_processing_thread():
     while not stop_event.is_set():
         img = cam.read()
         img = cv2.resize(img, (640, 480))
-        img = cv2.flip(img, 1)  # Flipping the image horizontally
-        cv2.imshow("Camera Feed", img)
-        cv2.waitKey(1)
+        img = cv2.flip(img, 1)
+
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        value = (11, 11)
+        blurred = cv2.GaussianBlur(hsv, value, 0)
+        colourLower = np.array([53, 55, 209])
+        colourUpper = np.array([180, 255, 255])
+
+        mask = cv2.inRange(blurred, colourLower, colourUpper)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))
+
+        height, width = img.shape[:2]
+
+        up_contour = mask[0:height//2, 0:width]
+        down_contour = mask[3*height//4:height, 2*width//5:3*width//5]
+
+        key_pressed = False
+
+        if len(cv2.findContours(up_contour, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]) > 0:
+            key_pressed, current_key = process_contours(
+                cv2.findContours(up_contour, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1],
+                current_key, A, A, 'LEFT'
+            )
+
+        if len(cv2.findContours(down_contour, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]) > 0:
+            key_pressed, current_key = process_contours(
+                cv2.findContours(down_contour, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1],
+                current_key, Space, Space, 'NITRO'
+            )
+
+            
 
     cam.stop()
     cv2.destroyAllWindows()
@@ -31,3 +60,4 @@ if __name__ == "__main__":
             pass
     except KeyboardInterrupt:
         stop_event.set()
+
