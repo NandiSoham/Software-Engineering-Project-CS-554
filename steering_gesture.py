6d5c91335f84bffc15a64a711eeb5d9527c2f340
NandiSoham <nandisoham21@gmail.com>
2023-11-14 17:07:42 -0800

    fixed the issue of color detection in right box

--- steering_gesture.py
+++ steering_gesture.py
@@ -21,7 +21,7 @@ def image_processing_thread():
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         value = (11, 11)
         blurred = cv2.GaussianBlur(hsv, value, 0)
-        colourLower = np.array([53, 55, 209])
+        colourLower = np.array([49, 102, 63])
         colourUpper = np.array([180, 255, 255])
 
         mask = cv2.inRange(blurred, colourLower, colourUpper)
@@ -37,16 +37,16 @@ def image_processing_thread():
 
         contours_up, _ = cv2.findContours(up_contour, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if len(contours_up) > 0:
-            key_pressed, current_key = contours_up(
+            key_pressed, current_key = process_contours(
                 contours_up,
-                current_key, KEY_A, KEY_A, 'LEFT'
+                current_key, KEY_A, KEY_A, 'LEFT', width
             )
 
         contours_down, _ = cv2.findContours(down_contour, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if len(contours_down) > 0:
-            key_pressed, current_key = contours_down(
+            key_pressed, current_key = process_contours(
                 contours_down,
-                current_key, KEY_SPACE, KEY_SPACE, 'NITRO'
+                current_key, KEY_D, KEY_D, 'RIGHT', width
             )
 
         img = cv2.rectangle(img, (0, 0), (width//2 - 35, height//2), (0, 255, 0), 1)
@@ -55,8 +55,6 @@ def image_processing_thread():
         img = cv2.rectangle(img, (width//2 + 35, 0), (width-2, height//2), (0, 255, 0), 1)
         cv2.putText(img, 'RIGHT', (440, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (139, 0, 0))
 
-        img = cv2.rectangle(img, (2*(width//5), 3*(height//4)), (3*width//5, height), (0, 255, 0), 1)
-        cv2.putText(img, 'NITRO', (2*(width//5) + 20, height-10), cv2.FONT_HERSHEY_DUPLEX, 1, (139, 0, 0))
 
         cv2.imshow("Camera Feed", img)
         cv2.waitKey(1)
@@ -68,6 +66,25 @@ def image_processing_thread():
 
     cam.stop()
     cv2.destroyAllWindows()
+
+def process_contours(contours, key_list, key_press, key_code, key_text, width):
+    key = False
+
+    if len(contours) > 0:
+        c = max(contours, key=cv2.contourArea)
+        M = cv2.moments(c)
+        cX = int(M["m10"] / (M["m00"] + 0.000001))
+
+        if cX < (width//2 - 35):
+            press_key(key_code)
+            key = True
+            key_list.append(key_code)
+        elif cX > (width // 2 + 35):
+            press_key(KEY_D)
+            key = True
+            key_list.append(KEY_D)
+
+    return key, key_list
 
 
 if __name__ == "__main__":
